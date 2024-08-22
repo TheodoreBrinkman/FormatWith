2fb3f13b4a6a69a2a6a3c8670060194ac7e1f6f9
Voice <53106311+TheodoreBrinkman@users.noreply.github.com>
2024-08-22 10:52:07 -0400

    Update FormatWithTests.cs
    Added test for left-pad formatting.

--- FormatWithTests/FormatWithTests.cs
+++ FormatWithTests/FormatWithTests.cs
@@ -171,6 +171,18 @@ namespace FormatWithTests
         }
 
         [Fact]
+        public void LeftPadTest()
+        {
+            Dictionary<string, object> replacementDictionary = new Dictionary<string, object>()
+            {
+                ["LeftPadTestValue"] = LeftPadTestValue
+            };
+            System.String expectedValue = "'       1,234.56'";
+            Assert.Equal(System.String.Format(LeftPadTestFormatComposite, LeftPadTestValue), exectedValue);
+            Assert.Equal(LeftPadTestValue.FormatWith(replacementDictionary), expectedValue);
+        }
+        
+        [Fact]
         public void SpeedTest()
         {
             Dictionary<string, string> replacementDictionary = new Dictionary<string, string>()
